958f567e8ce58dfca86d3e67eb39b3d6f964744a
Simon Zeni <simonzeni@gmail.com>
2018-05-16 22:19:44 -0300

    Add desktop pointer to window

--- bspwc/window.c
+++ bspwc/window.c
@@ -1,14 +1,15 @@
 #include "bspwc/window.h"
 
-struct window* create_window()
+struct window* create_window(struct desktop* desktop)
 {
-    struct window* window = calloc(1, sizeof(struct window));
-    if (window == NULL)
-    {
-        return NULL;
-    }
+	struct window* window = calloc(1, sizeof(struct window));
+	if (window == NULL)
+	{
+		return NULL;
+	}
 
-    window->alpha = 1.0f;
+	window->desktop = desktop;
+	window->alpha = 1.0f;
 
-    return window;
+	return window;
 }
